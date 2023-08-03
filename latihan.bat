359899e9bf5b5d4fbaa8db1c403e7d1ce4a25b19
siswo27 <137741383+siswo27@users.noreply.github.com>
2023-08-03 21:32:03 +0700

    latihan.bat

---
+++ latihan.bat
@@ -0,0 +1,21 @@
+@echo off
+title Latihan !
+rem =====Variabel======
+set P=50
+set L=60
+
+rem ======tampilan
+echo -------------------------------------
+echo Nim 		:20122078
+echo Nama 		:Siswo Harjo
+echo Kelas 		:TI 2B
+echo Jurusan		:Teknik Informatika
+echo -------------------------------------
+rem ====== rumus =========
+echo Diketahui
+set /a V= P*L
+echo panjang		:%P%
+echo lebar		:%L%
+echo -------------------------------------
+echo Luas bangunan= %V%
+pause
